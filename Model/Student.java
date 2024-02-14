98dc69bab8a4fb4a276dbb79fbc3b378377b25f5
ERos1012 <65523315+ERos1012@users.noreply.github.com>
2024-02-14 10:59:42 -0500

    Added Student and CourseRecord Objects

---
+++ Model/Student.java
@@ -0,0 +1,35 @@
+package Model;
+
+/**
+ * Student
+ */
+public class Student {
+    private String name;
+    private int id;
+    private String program;
+    private String email;
+    private String classification;
+
+    public Student(String name, int age, int id, String program, String email) {
+        this.name = name;
+        this.id = id;
+        this.program = program;
+        this.email = email;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
---
+++ Model/StudentCourseRecord.java
@@ -0,0 +1,53 @@
+package Model;
+
+public class StudentCourseRecord {
+    private int studentId;
+    private String courseCode;
+    private String grade;
+    private String semester;
+    private int year;
+
+    public StudentCourseRecord(int studentId, String courseCode, String grade, String semester, int year) {
+        this.studentId = studentId;
+        this.courseCode = courseCode;
+        this.grade = grade;
+        this.semester = semester;
+        this.year = year;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String toString() {
+        return "Student ID: " + studentId + ", Course Code: " + courseCode + ", Grade: " + grade + ", Semester: " + semester + ", Year: " + year;
+    }
+}
