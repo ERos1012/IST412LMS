c75447243c324140c3617503f82436a2255332a5
“bzhoupsu” <zhoupeiqiao@gmail.com>
2024-03-02 18:16:45 -0500

    testharness created

---
+++ testharness/testharness.java
@@ -0,0 +1,11 @@
+/*should consist of a) controller components passing control between themselves as envisioned in your design, and
+b) controller components calling the APIs of their view and model components, again as envisioned in your design.
+*/
+
+public class testharness {
+
+    public static void main(String[] args) {
+
+    }
+}
+
