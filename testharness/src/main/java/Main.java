e2c064f3485a1fe931f4d768cf882ad9433d7632
“bzhoupsu” <zhoupeiqiao@gmail.com>
2024-03-02 19:06:43 -0500

    testharness main

---
+++ testharness/src/main/java/Main.java
@@ -0,0 +1,14 @@
+/*should consist of a) controller components passing control between themselves as envisioned in your design, and
+b) controller components calling the APIs of their view and model components, again as envisioned in your design.
+
+The testharness module should include a main method and be runnable. When it is run it should execute a series of
+tests with appropriate output statements communicating whether the tests have passed or failed. The test harness
+module will need to instantiate at least one controller class to begin the test, which in turn instantiate other
+controller, view, and model classes and exercise their APIs. You may want to include test output statements in your
+method stubs to reduce coupling between the testharness module and your other modules.
+*/
+
+public class Main
+{
+
+}
